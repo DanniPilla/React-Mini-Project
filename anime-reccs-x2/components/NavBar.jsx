f509007bb692395e2b3880411912abe2a227c633
DanniPilla <daniellapilla08@gmail.com>
2024-11-19 15:06:54 +1100

    yes

--- anime-reccs-x2/components/NavBar.jsx
+++ anime-reccs-x2/components/NavBar.jsx
@@ -9,6 +9,7 @@ const {theme} = useContext(MyThemeContext);
 return (
 <nav 
   className="NavBar sticky  top-0  w-full  flex justify-between items-center shadow-md bg-white  z-50">
+    <p className="pl-4">Anime Finder</p> 
   <ul className="flex justify-center space-x-4 py-4">
     <li><NavLink to="/" className="px-4 uppercase font-bold text-pink-500 hover:text-gray-500" >Home</NavLink></li>
     <li><NavLink to="/anime" className="px-4 uppercase font-bold text-pink-500 hover:text-gray-500" >Anime</NavLink></li>
--- anime-reccs-x2/pages/HomePage.jsx
+++ anime-reccs-x2/pages/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage(){
    const { theme } = useContext(MyThemeContext);
  return (
     <div
-      className="w-full min-h-screen  flex flex-col justify-center "
+      className="w-full min-h-screen  flex flex-col justify-center items-center "
       style={{
         backgroundColor: theme.background,
         color: theme.foreground,
@@ -22,7 +22,7 @@ export default function HomePage(){
         Discover your favorite anime, explore genres, and build your watchlist.
         Dive into the world of anime with just a few clicks!
       </p>
-      <img className="mt-[2rem] max-w-full h-auto" src="https://64.media.tumblr.com/6fb49796364519b50627ef20d1ee9bfd/989ae70bbd8fd464-61/s500x750/bf1ba1d902fe3b62ef611e4ecee843fa8417cdeb.gifv"/>
+      <img className="mt-[2rem] w-20" src="https://64.media.tumblr.com/6fb49796364519b50627ef20d1ee9bfd/989ae70bbd8fd464-61/s500x750/bf1ba1d902fe3b62ef611e4ecee843fa8417cdeb.gifv"/>
     </div>
   );
 }
